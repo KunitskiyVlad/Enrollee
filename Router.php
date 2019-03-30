cb6688f3ee0c77f264e5982e79957a6014fd8aae
KunitskiyVlad <kunitskiyvlad1@gmail.com>
2019-03-30 20:49:55 +0200

    change Router file and change abstract controller

--- Router.php
+++ Router.php
@@ -1,43 +1,8 @@
 <?php
-use App\Controller as controller;
-use App\HTTP\Request;
-	class Router {
-		public static function start(Request $request)
-		{
-
-			$controller ='home';
-			$action = 'index';
-			$url = parse_url($request->getUri());
-			//echo($url['path']);
-			if(!empty($url['path']) && $url['path'] != '/')
-
-			{
-				$controller = str_replace('/', '', $url['path']);
-			}
-			
-			if(!empty($url[2]))
-			
-			{
-				$action = $url[2];
-			}
-			
-			$controller_class = 'App\\Controller\\'.mb_strtolower('controller_'.$controller);
-			$action = mb_strtolower('action_'.$action);
-			//if(file_exists('controller/'.$controller_class.'.php') )
-			//{
-				//include 'controller/'.$controller_class.'.php';
-				$class = new $controller_class;
-				$class->$action();
-			//}
-			//else 
-			//{
-				//Router::error();
-			//}
-		}
-
-		public static function error()
-		{
-			header('HTTP/1.1 404 Not Found');
-			die();
-		}
-	}
+use App\Http\RouteCollection;
+use App\Http\Router;
+$routes = new RouteCollection();
+$routes->get('home','/','controller_home@index');
+$routes->get('registration','/registration','controller_registration@index');
+$routes->get('cabinet','/cabinet','controller_cabinet@index');
+$rout = new Router($routes);
--- controller/controller.php
+++ controller/controller.php
@@ -1,20 +1,37 @@
 <?php
 namespace App\Controller;
+use App\Http\Request;
 use App\View\view;
 
-	class controller 
+	abstract class controller
 		{
 
 			public $view;
-			private $model;
 
 			function __construct()
 			{
 				$this->view = new view;
 			}
-			public function action_index()
+			public function index()
 			{
 				
 			}
 
+			public function show(){
+
+            }
+
+            public function create(Request $request)
+            {
+
+            }
+            public function delete(Request $request)
+            {
+
+            }
+
+            public function store(Request $request)
+            {
+
+            }
 		}	
